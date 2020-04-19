24fdbc8c4a37b12f29421307133c89e8ec61fd2d
TrideepLD <42204195+TrideepLD@users.noreply.github.com>
2020-04-19 15:41:48 +1000

    Update Client.java
    Changed deprecated? depreceted? deprecated thingy which was DataInputStream

--- Client.java
+++ Client.java
@@ -1,16 +1,47 @@
-// A Java program for a Client 
+m for a Client 
 import java.net.*; 
 import java.io.*; 
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.util.*;
+import java.xml
   
 public class Client 
 { 
-    // initialize socket and input output streams 
+	/* 
+	Initialize socket and input output streams 
+	Changed DataInputStream to BufferedReader due to deprecation in code
+    BufferedReader is supposed to get info from the socket
+	DataOutputStream is thus used to write to the socket */
+    
     private Socket socket            = null; 
-    private DataInputStream  input   = null; 
+    private BufferedReader  in       = null; 
     private DataOutputStream out     = null; 
-  
-    BufferedReader brin = new BufferedReader(new InputStreamReader(new DataInputStream(System.in)));
 
+
+    // Print the element
+    public static void PrintElement(Element elmnt, String name){
+        System.out.println(elmnt.getNodeName());
+        NodeList nodes = elmnt.getElementsByTagName(name);
+        PrintAttribute(nodes);
+    }
+
+    //Print the attributes
+    public static void PrintAttribute(NodeList nodes){
+        for(int i = 0; i< nodes.getLength(); i++){
+            Node child  = nodes.item(i);
+            System.out.print("Node: " + child.getNodeName());
+            if(child.getNodeType() == Node.ELEMENT_NODE){
+                NamedNodeMap attribs = child.getAttributes();
+                for(int y = 0; y < attribs.getLength(); y++){
+                    Node temp = attribs.item(y);
+                    System.out.print(" " + temp.getNodeName() + "=" + temp.getNodeValue());
+                }
+                System.out.println("");
+            }
+        }
+    }
+  
     // constructor to put ip address and port 
     public Client(String address, int port) 
     { 
@@ -18,23 +49,26 @@ public class Client
         try
         { 
             socket = new Socket(address, port); 
-            System.out.println("Connected"); 
+            System.out.println("CONNECTED!!!!!"); 
   
             // takes input from terminal 
-             
-  
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // can also use a reader and call bufferedReader(inputStreamReader(Reader))
+
             // sends output to the socket 
-            out    = new DataOutputStream(socket.getOutputStream()); 
+            out = new DataOutputStream(socket.getOutputStream()); 
         } 
         catch(UnknownHostException u) 
         { 
+        	System.out.println("There is an Unknown Host Exception"); 
             System.out.println(u); 
         } 
         catch(IOException i) 
         { 
+        	System.out.println("IO Exception. Quickly! Unplug and plug it back in!"); 
             System.out.println(i); 
         } 
-  
+
         // string to read message from input 
         String line = ""; 
   
@@ -43,11 +77,12 @@ public class Client
         { 
             try
             { 
-                line = brin.readLine();
+                line = in.readLine(); 
                 out.writeUTF(line); 
             } 
             catch(IOException i) 
             { 
+            	System.out.println("This IO Exception is when line!=Over")
                 System.out.println(i); 
             } 
         } 
@@ -55,7 +90,7 @@ public class Client
         // close the connection 
         try
         { 
-            input.close(); 
+            in.close(); 
             out.close(); 
             socket.close(); 
         } 
@@ -63,11 +98,34 @@ public class Client
         { 
             System.out.println(i); 
         } 
-    } 
-  
+    }
+
     public static void main(String args[]) 
     { 
-        Client client = new Client("127.0.0.1", 5000); 
+        Client client = new Client("127.0.0.1", 50000);
+        try {
+            File inputFile = new File("system.xml");
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+
+            doc.getDocumentElement().normalize();
+
+            Element root  = doc.getDocumentElement();
+
+            Element server_root = (Element)root.getElementsByTagName("servers").item(0);
+            Element job_root = (Element)root.getElementsByTagName("jobs").item(0);
+            NodeList workload = root.getElementsByTagName("workload");
+            Element termination_root = (Element)root.getElementsByTagName("termination").item(0);
+
+            PrintElement(server_root, "server");
+            PrintElement(job_root, "job");
+            PrintAttribute(workload);
+            PrintElement(termination_root, "condition");
+
+        } catch(Exception e){
+            e.printStackTrace();
+        } 
     } 
 } 
-
