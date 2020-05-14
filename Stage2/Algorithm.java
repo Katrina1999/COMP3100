852ae3228a437ed1623beda2d6fd885ab790eee1
sakurajm <62170545+sakurajm@users.noreply.github.com>
2020-05-14 11:00:58 +1000

    All files to run Worst-fit algorithm

---
+++ Stage2/Algorithm.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import Folder.Server; 
+import Folder.Job; 
+
+public class Algorithm {
+	private ArrayList<Server> servers = new ArrayList<Server>();
+	private Server[] XMLServers;
+
+	Algorithm(ArrayList<Server> servers, Server[] XMLServers) {
+		this.servers = servers;
+		this.XMLServers = XMLServers;
+	}
+
+
+	//Worst-fit algorithm implemented by Sakura Mukhopadhyay
+	public Server worstFit(Job currjob) {
+		  // Defining variables  
+		  int worstFit = Integer.MIN_VALUE;
+		  int altFit = Integer.MIN_VALUE;
+		  Server worst = null;
+		  Server alt = null;
+		  Boolean worstFound = false;
+		  Boolean altFound = false;
+
+		  for (Server serv: servers) {
+			    if (serv.coreCount >= currjob.coreCount && serv.disk >= currjob.disk && serv.memory >= currjob.memory
+					&& (serv.state == 0 || serv.state == 2 || serv.state == 3)) {
+				    int fitnessValue = serv.coreCount - currjob.coreCount;
+				    if (fitnessValue > worstFit && (serv.availableTime == -1 || serv.availableTime == currjob.bootupTime)) {
+					        worstFit = fitnessValue;
+					        worstFound = true;
+					        worst = serv;
+				         } else if (fitnessValue > altFit && serv.availableTime >= 0) {
+					    altFit = fitnessValue;
+					  altFound = true;
+					alt = serv;
+				}
+			}
+		}
+    
+		// Return the server most suitable or 'worst-fit' and with the highest fitnesss value 
+		if (worstFound) {
+			return worst;
+		} else if (altFound) {
+			return alt;
+		}
+		int lowest = Integer.MIN_VALUE; //sets the value of lowest to a very large number
+		Server current = null;
+		for (Server serv: XMLServers) {
+			int fit = serv.coreCount - currjob.coreCount;
+			if (fit > lowest && serv.disk >= currjob.disk && serv.memory >= currjob.memory) {
+				lowest = fit;
+				current = serv;
+			}
+		}
+		current.id = 0; //sets the current id of the alternative server to 0
+		return current; ////returns the current server (worstFit active server) based on highest resource capacity 
+		}
+	}
+}
