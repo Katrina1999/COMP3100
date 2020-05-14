8038874e637228e4c21bdada3173d30e076b9651
TrideepLD <trideeplaldas@hotmail.com>
2020-05-14 17:17:59 +1000

    Changes

--- Stage2/ds-sim/Folder/Server.java
+++ Stage2/ds-sim/Folder/Server.java
@@ -16,7 +16,7 @@ public class Server {
 	public int state;
 	public int availableTime;
 
-	
+
 	public Server(int id, String t, int l, int b, float r, int c, int m, int d) {
 		this.id = id;
 		this.type = t;
@@ -27,9 +27,9 @@ public class Server {
 		this.memory = m;
 		this.disk = d;
 	}
-	
 
-	
+
+
 	public Server(int id, String t, int l, int b, float r, int c, int m, int d, int state, int availableTime) {
 		this.id = id;
 		this.type = t;
@@ -42,52 +42,5 @@ public class Server {
 		this.state = state;
 		this.availableTime = availableTime;
 	}
-	
+
 }
-<<<<<<< HEAD
-=======
-package Folder;
-
-public class Server {
-	public int id;
-	public String type;
-	public int limit;
-	public int bootupTime;
-	public float rate;
-	public int coreCount;
-	public int memory;
-	public int disk;
-	public int state;
-	public int availableTime;
-
-	
-	public Server(int id, String t, int l, int b, float r, int c, int m, int d) {
-		this.id = id;
-		this.type = t;
-		this.limit = l;
-		this.bootupTime = b;
-		this.rate = r;
-		this.coreCount = c;
-		this.memory = m;
-		this.disk = d;
-	}
-	
-
-	
-	public Server(int id, String t, int l, int b, float r, int c, int m, int d, int state, int availableTime) {
-		this.id = id;
-		this.type = t;
-		this.limit = l;
-		this.bootupTime = b;
-		this.rate = r;
-		this.coreCount = c;
-		this.memory = m;
-		this.disk = d;
-		this.state = state;
-		this.availableTime = availableTime;
-	}
-	
-}
->>>>>>> First-fit
-=======
->>>>>>> origin/Worst-fit
