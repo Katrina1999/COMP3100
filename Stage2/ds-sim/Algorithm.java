f139af921259b13cc967a05a59ea8505a246b868
TrideepLD <trideeplaldas@hotmail.com>
2020-05-14 17:20:22 +1000

    Update Algorithm.java

--- Stage2/ds-sim/Algorithm.java
+++ Stage2/ds-sim/Algorithm.java
@@ -11,6 +11,101 @@ public class Algorithm {
 		this.XMLServers = XMLServers;
 	}
 
+
+	/*
+	*	Best-Fit Algorithm
+	*/
+
+
+	//BestFit Algorithm implemented by Katrina David
+	public Server bestFit(Job currJob){
+		int bestFit = Integer.MAX_VALUE; //sets the value of bestFit to a very large number
+		int minAvail = Integer.MAX_VALUE; //sets the value of minAvail to a very large number
+		Server best = null;
+		Boolean found = false; //sets the value of found (indicates if bestFit has been found) to false
+
+		for(Server serv: servers){ //for every server type in the order that it appears in the xml do the following
+			if((serv.coreCount >= currJob.coreCount && serv.disk >= currJob.disk && serv.memory >= currJob.memory)){
+				int fitnessValue = serv.coreCount - currJob.coreCount;
+				if((fitnessValue < bestFit) || (fitnessValue == bestFit && serv.availableTime < minAvail)){ //if the server has sufficient available resources to run job j then
+					bestFit = fitnessValue; //calculates the fitnessValue
+					minAvail = serv.availableTime; //sets the minimum time available to that of the available time of the server
+					if(serv.state == 0 || serv.state == 1 || serv.state == 2 || serv.state == 3){
+						found = true; //bestFit has been found as such it has been set to true
+						best = serv; //sets the best server to the value of server
+					}
+				}
+			}
+		}
+		if(found) { //if the bestFit is found then do the following
+			return best; //returns the server best because it has been found
+		} else {
+			int bestFitOther = Integer.MAX_VALUE; //sets the value of bestFitOther to a very large number
+			Server servAlt = null; //sets the current value of the alternative server to null
+			for (Server serv : XMLServers){ //goes through the server against all the XML servers
+				int fitnessValueOther = serv.coreCount - currJob.coreCount; //sets the value of fitnessValueOther to that of the difference between the current server's coreCount and the current job's coreCount
+				if(fitnessValueOther >= 0 && fitnessValueOther < bestFitOther && serv.disk > currJob.disk & serv.memory > currJob.memory) {
+					bestFitOther = fitnessValueOther; //sets the value of bestFitOther to equal the other fitnessValue found
+					servAlt = serv; //sets the value of the alternative server to that of the current server
+				}
+			}
+			servAlt.id = 0; //sets the current id of the alternative server to 0
+			return servAlt; //returns the alternative server (bestFit active server) based on initial resource capacity
+		}
+
+
+	}
+
+
+	/*
+	*	Worst-Fit Algorithm
+	*/
+
+
+	//Worst-fit algorithm implemeneted by Sakura Mukhopadhyay
+	public Server worstFit(Job currjob) {
+		// Establish flags and fit variables to track fitness scores and servers.
+		int worstFit = Integer.MIN_VALUE;
+		int altFit = Integer.MIN_VALUE;
+		Server worst = null;
+		Server alt = null;
+		Boolean worstFound = false;
+		Boolean altFound = false;
+
+		for (Server serv: servers) {
+			if (serv.coreCount >= currjob.coreCount && serv.disk >= currjob.disk && serv.memory >= currjob.memory
+					&& (serv.state == 0 || serv.state == 2 || serv.state == 3)) {
+				int fitnessValue = serv.coreCount - currjob.coreCount;
+				if (fitnessValue > worstFit && (serv.availableTime == -1 || serv.availableTime == currjob.bootupTime)) {
+					worstFit = fitnessValue;
+					worstFound = true;
+					worst = serv;
+				} else if (fitnessValue > altFit && serv.availableTime >= 0) {
+					altFit = fitnessValue;
+					altFound = true;
+					alt = serv;
+				}
+			}
+		}
+		// Return the server most suitable
+		if (worstFound) {
+			return worst;
+		} else if (altFound) {
+			return alt;
+		}
+		int lowest = Integer.MIN_VALUE;
+		Server current = null;
+		for (Server serv: XMLServers) {
+			int fit = serv.coreCount - currjob.coreCount;
+			if (fit > lowest && serv.disk >= currjob.disk && serv.memory >= currjob.memory) {
+				lowest = fit;
+				current = serv;
+			}
+		}
+		current.id = 0; // If not zero, server thinks it doesn't exist
+		return current;
+	}
+
 	/*
 	*	First Fit Algorithm
 	*/
@@ -64,54 +159,6 @@ public class Algorithm {
 			}
 		}
 		return servArr;
-	}
-
-
-	/*
-	*	Worst-Fit Algorithm
-	*/
-	//Worst-fit algorithm implemeneted by Sakura Mukhopadhyay
-	public Server worstFit(Job currjob) {
-		// Establish flags and fit variables to track fitness scores and servers.
-		int worstFit = Integer.MIN_VALUE;
-		int altFit = Integer.MIN_VALUE;
-		Server worst = null;
-		Server alt = null;
-		Boolean worstFound = false;
-		Boolean altFound = false;
-
-		for (Server serv: servers) {
-			if (serv.coreCount >= currjob.coreCount && serv.disk >= currjob.disk && serv.memory >= currjob.memory
-					&& (serv.state == 0 || serv.state == 2 || serv.state == 3)) {
-				int fitnessValue = serv.coreCount - currjob.coreCount;
-				if (fitnessValue > worstFit && (serv.availableTime == -1 || serv.availableTime == currjob.bootupTime)) {
-					worstFit = fitnessValue;
-					worstFound = true;
-					worst = serv;
-				} else if (fitnessValue > altFit && serv.availableTime >= 0) {
-					altFit = fitnessValue;
-					altFound = true;
-					alt = serv;
-				}
-			}
-		}
-		// Return the server most suitable
-		if (worstFound) {
-			return worst;
-		} else if (altFound) {
-			return alt;
-		}
-		int lowest = Integer.MIN_VALUE;
-		Server current = null;
-		for (Server serv: XMLServers) {
-			int fit = serv.coreCount - currjob.coreCount;
-			if (fit > lowest && serv.disk >= currjob.disk && serv.memory >= currjob.memory) {
-				lowest = fit;
-				current = serv;
-			}
-		}
-		current.id = 0; // If not zero, server thinks it doesn't exist
-		return current;
 	}
 
 }
