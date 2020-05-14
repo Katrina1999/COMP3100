d8a1d4579428d92a005323b3a6d1c15a303dd9e6
sakurajm <62170545+sakurajm@users.noreply.github.com>
2020-05-14 14:46:56 +1000

    Update Algorithm.java

--- Stage2/Algorithm.java
+++ Stage2/Algorithm.java
@@ -22,14 +22,14 @@ public class Algorithm {
 		  Boolean worstFound = false;
 		  Boolean altFound = false;
 
-		  for (Server serv: servers) {
+		  for (Server serv: servers) { //for every server type in the order that it appears in the xml do the following
 			    if (serv.coreCount >= currjob.coreCount && serv.disk >= currjob.disk && serv.memory >= currjob.memory
 					&& (serv.state == 0 || serv.state == 2 || serv.state == 3)) {
 				    int fitnessValue = serv.coreCount - currjob.coreCount;
-				    if (fitnessValue > worstFit && (serv.availableTime == -1 || serv.availableTime == currjob.bootupTime)) {
-					        worstFit = fitnessValue;
-					        worstFound = true;
-					        worst = serv;
+				    if (fitnessValue > worstFit && (serv.availableTime == -1 || serv.availableTime == currjob.bootupTime)//if the server has more available resources than what is required to run job j then) {
+					        worstFit = fitnessValue; // fitness value is calculated
+					        worstFound = true;	//worstFit has been found as such it has been set to true
+					        worst = serv;		////sets the worst server to the value of server
 				         } else if (fitnessValue > altFit && serv.availableTime >= 0) {
 					    altFit = fitnessValue;
 					  altFound = true;
@@ -39,22 +39,22 @@ public class Algorithm {
 		}
     
 		// Return the server most suitable or 'worst-fit' and with the highest fitnesss value 
-		if (worstFound) {
-			return worst;
-		} else if (altFound) {
+		if (worstFound) { //if the worstFit is found then do the following:
+			return worst;  //returns the server with the best 'worst-fit' value 
+		} else if (altFound) { //if not found we return the next server available and do the following:
 			return alt;
 		}
-		int lowest = Integer.MIN_VALUE; //sets the value of lowest to a very large number
-		Server current = null;
-		for (Server serv: XMLServers) {
-			int fit = serv.coreCount - currjob.coreCount;
-			if (fit > lowest && serv.disk >= currjob.disk && serv.memory >= currjob.memory) {
-				lowest = fit;
-				current = serv;
+		int lowest = Integer.MIN_VALUE; //sets the value of lowest to a very small number
+		Server current = null;		//sets the current value of the current server to null 
+		for (Server serv: XMLServers) {	//goes through the current server and compares against all the XML servers 
+			int fit = serv.coreCount - currjob.coreCount; //sets the value of fitness value to that of the difference between the current server's coreCount and the current job's coreCount
+			if (fit > lowest && serv.disk >= currjob.disk && serv.memory >= currjob.memory) { //it then compares if the fitness value is greater than the current job's memory, disk and coreCount size
+				lowest = fit; //the highest fitness value is selected and we set the previous value of lowest to this
+				current = serv; //the previous current server (alternative server) is now set to the current server
 			}
 		}
-		current.id = 0; //sets the current id of the alternative server to 0
-		return current; ////returns the current server (worstFit active server) based on highest resource capacity 
+		current.id = 0; //sets the current id of the alternative(previously current) server to 0
+		return current; //returns the current server (worstFit active server) based on highest resource capacity 
 		}
 	}
 }
